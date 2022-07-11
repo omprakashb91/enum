784afe17f07527719501e587dc00b6d346cc1423
omprakashb91 <79237022+omprakashb91@users.noreply.github.com>
2022-07-11 23:59:20 +0530

    enum _binary sysem

--- enum_tutorial.c
+++ enum_tutorial.c
@@ -1,14 +1,15 @@
 /*
-Date: 06 July 2022
-Aim: To demonstrate enum
+Date: 11 July 2022
+Aim: To demonstrate enum using ON and OFF switch
 Author: Omprakash
 */
 #include<stdio.h>
 int main()
 {
-enum planets {MERCURY=1, VENUS, EARTH, MARS,JUPITER, SATURN, URANUS, NEPTUE, PLUTO};
-enum planets firstplanet = MERCURY;
-enum planets secondplanet = VENUS;
-printf("firstplanet: %d\n secondplanet: %d",firstplanet,secondplanet);
-return 0;
+    enum by {ZERO,ONE};
+    enum by OFF=ZERO;
+    enum by ON=ONE;
+    printf("digital value of ON is = %d\n",ON);
+    printf("digital value of OFF is = %d\n",OFF);
+    return 0;
 }
